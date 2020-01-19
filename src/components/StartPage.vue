f8403d28801ba4f7e453b09e5bdba7f550a3c973
LinetTheFox <linetmoreandil@gmail.com>
2020-01-19 16:06:12 +0200

    Fixed StartPage component

--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -2,75 +2,70 @@
     <div id="app">
         <v-app id="inspire">
             <v-content>
-                <v-container
-                        fluid
-                        fill-height
-                >
-                    <v-layout
-                            align-center
-                            justify-center
-                    >
-                        <v-flex
-                                xs12
-                                sm8
-                                md4
-                        >
-                            <v-card class="elevation-12">
-                                <v-toolbar
-                                        color="primary"
-                                        dark
-                                        flat
-                                >
-                                    <v-toolbar-title>Start page</v-toolbar-title>
-                                </v-toolbar>
-                                <v-card-text>
-                                    <v-toolbar>
-                                        <v-tabs
-                                                dark
-                                                background-color="primary"
-                                                grow
-                                        >
-                                            <v-tab>
-                                                Input lirycs
-                                            </v-tab>
+                <v-container fluid fill-height>
+                    <v-layout align-center justify-center>
+                        <v-flex xs12 sm8 md4>
+                            <v-card class="elevation-24">
+                                <v-toolbar color="primary" dark flat>
+                                    <v-container>
+                                        <v-layout align-center justify-end>
+                                            <v-flex xs12 sm8 md5/>
+                                            <v-flex xs12 sm8 md5>
+                                                <v-toolbar-title>Start page</v-toolbar-title>
+                                            </v-flex>
+                                            <v-flex xs12 sm8 md2>
+                                                <v-btn color="primary">Play!</v-btn>
+                                            </v-flex>
+                                        </v-layout>
+                                    </v-container>
 
+                                </v-toolbar>
+                                <v-card>
+                                    <v-toolbar>
+                                        <v-tabs dark background-color="primary" grow slider-color="yellow">
                                             <v-tab>
-                                                Record sound
+                                                Input lyrics
+                                            </v-tab>
+                                            <v-tab>
+                                                Record
                                             </v-tab>
 
                                             <v-tab-item>
-
-                                                <v-textarea
-                                                        color="teal"
-                                                ><!--TODO: BIND V-MODEL, fix css-->
-                                                    <template v-slot:label>
-                                                        <div>
-                                                            Lirycs
-                                                        </div>
-                                                    </template>
-                                                </v-textarea>
-
+                                                <v-card>
+                                                    <v-textarea color="teal">
+                                                        <!--TODO: BIND V-MODEL, fix css-->
+                                                        <template v-slot:label>
+                                                            <div>
+                                                                Lirycs
+                                                            </div>
+                                                        </template>
+                                                    </v-textarea>
+                                                </v-card>
                                             </v-tab-item>
 
                                             <v-tab-item>
-                                                <audio-recorder
-                                                        upload-url="some url"
-                                                        :attempts="3"
-                                                        :time="2"
-                                                        :before-recording="callback"
-                                                        :after-recording="callback"
-                                                        :before-upload="callback"
-                                                        :successful-upload="callback"
-                                                        :failed-upload="callback"/>
+                                                <v-container>
+                                                    <v-layout align-center justify-center>
+                                                        <v-flex xs12 sm8 md10>
+                                                            <v-card class="audio-recorder" flat>
+                                                                <audio-recorder
+                                                                        upload-url="some url"
+                                                                        :attempts="3"
+                                                                        :time="2"
+                                                                        :before-recording="callback"
+                                                                        :after-recording="callback"
+                                                                        :before-upload="callback"
+                                                                        :successful-upload="callback"
+                                                                        :failed-upload="callback"/>
+                                                            </v-card>
+                                                        </v-flex>
+                                                    </v-layout>
+                                                </v-container>
                                             </v-tab-item>
 
                                         </v-tabs>
                                     </v-toolbar>
-                                </v-card-text>
-                                <v-card-actions>
-                                    <v-spacer></v-spacer>
-                                    <v-btn color="primary">Play!</v-btn>
-                                </v-card-actions>
+                                </v-card>
                             </v-card>
                         </v-flex>
                     </v-layout>
@@ -92,5 +87,11 @@
 </script>
 
 <style scoped>
+    .elevation-24 {
+        top: -250px
+    }
 
+    .audio-recorder {
+        align-items: center;
+    }
 </style>
