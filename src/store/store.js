21cd5aae83fdb8c077af1b5075683ee3786c1612
BaLiKfromUA <yuhimenko.valentin@gmail.com>
2020-01-22 23:19:29 +0200

    vuex persisted state

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import 'es6-promise/auto';
 import Vuex from 'vuex';
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+    plugins: [createPersistedState()],
     state: {
         stage: "start",
         tracks: [],
